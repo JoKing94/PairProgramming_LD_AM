eb971f733799a4e2c02252ed2160375b0efed5d0
JoKing94 <joseph94.king@gmail.com>
2020-02-11 10:13:58 +0100

    la base

---
+++ PairProgramming_LD_AM/src/main/java/com/scholanova/PairProgramming_LD_AM/PairProgrammingLdAmApplication.java
@@ -0,0 +1,13 @@
+package com.scholanova.PairProgramming_LD_AM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PairProgrammingLdAmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PairProgrammingLdAmApplication.class, args);
+	}
+
+}
---
+++ PairProgramming_LD_AM/src/test/java/com/scholanova/PairProgramming_LD_AM/PairProgrammingLdAmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scholanova.PairProgramming_LD_AM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PairProgrammingLdAmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
